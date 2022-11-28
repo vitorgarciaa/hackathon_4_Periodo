003d72b9e504e2aaa15cfd33e999e9f8ed64d58f
VitorGarcia-Collab <lvitor2424@gmail.com>
2022-11-28 02:11:03 -0300

    Finalizacao da tela de apresentacao

--- src/pages/Info/index.tsx
+++ src/pages/Info/index.tsx
@@ -4,9 +4,25 @@ export const Info = () => {
     // Exibição da apresentação
     return (
         <div className='container'>
-            <h1>tela De Apresentação Do Hackathon</h1>
+            <h1>Bem Vindo!!</h1>
+
+
+
+            <h3>Esse sistema serve para venda de HQ's raras da Marvel. Espero que goste do nosso site, fique a vontade!!</h3>
+
+            <div className="list-group">
+             <a href="#" className="list-group-item list-group-item-action active" aria-current="true">
+            Lista de Funcionalidades
+            </a>
+             <a href="#" className="list-group-item list-group-item-action">1. Buscar os dados da API</a>
+            <a href="#" className="list-group-item list-group-item-action">2. Trazer as imagens das HQ's na tela, e o valor de cada</a>
+            <a href="#" className="list-group-item list-group-item-action">3. Trazer as informações sobre as HQ's, trazendo o valor e a disponibilidade </a>
+        </div>
+
+        <h3>Clique no botao abaixo para ser redirecionado!</h3>
             
                <Link to="/home" className='btn btn-primary'>Sistema</Link>
+
              
         </div>
     );
